78eb20a65166c689c1905c16c5dab6c105d3b1e8
Yunus ÖZCAN <me@yunusozcan.com>
2024-09-29 18:00:31 +0300

    js to ts

--- css.d.ts
+++ css.d.ts
@@ -1,4 +1,3 @@
-declare module '*.module.css' {
-  const classes: Record<string, string>;
-  export default classes;
+declare module 'csstype' {
+  type Properties = Record<`--${string}`, string>;
 }
